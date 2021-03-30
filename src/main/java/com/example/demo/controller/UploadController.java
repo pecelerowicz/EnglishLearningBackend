64cb80522b8562a2105e70a0e355eb8eb34e4f2a
Michal Pecelerowicz <michal.pecelerowicz@gmail.com>
2021-03-30 15:12:04 +0200

    Add @CrossOrigin

--- src/main/java/com/example/demo/controller/UploadController.java
+++ src/main/java/com/example/demo/controller/UploadController.java
@@ -1,16 +1,13 @@
 package com.example.demo.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api")
 public class UploadController {
